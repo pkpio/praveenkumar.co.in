ac74b05977270c879b662977fc467fe2506e0f12
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-07-22 04:14:45 +0530

    Some repositioning in works. In perfect place.

--- content/Works.php
+++ content/Works.php
@@ -1,3 +1,3 @@
-<div style="position:relative; top:-35px; left:32px; height:280px; overflow:hidden;">
-<iframe src="git.html" width="80%" height="1000px" style="border:none; padding-left:5%;" scrolling="yes"></iframe>
+<div style="position:relative; top: 66px; left: 30px; height: 321px; overflow-x: hidden;overflow-y: scroll;width: 475px;">
+<iframe src="git.html" width="80%" height="1000px" style="border:none; padding-left: 0%;width: 426px;padding: 0px 0px 0px 30px;height: 657px;" scrolling="yes"></iframe>
 </div>
