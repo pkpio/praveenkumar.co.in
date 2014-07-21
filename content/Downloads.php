8aa7ad3ef0bebabb2fc1f51af05f67c6e3be2279
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-07-22 03:40:57 +0530

    Updated downloads section to use files sub-domain

--- content/Downloads.php
+++ content/Downloads.php
@@ -1,39 +1,96 @@
+<?php
+function _format_bytes($size)
+{
+    if ($size < 1024) {
+		$sizeString = $size." Bytes";
+    } elseif ($size < 1048576) {
+		$sizeString = round($size/1024, 2)." KB";
+    } else {
+		$sizeString = round($size/1048576, 2)." MB";
+    }
+	return $sizeString;
+}
+
+function isDownloadFile($file){
+	$flag = false;
+	
+	// -TODO- Make an array of non-allowed files and loop
+	if($file != ".")
+		$flag = true;
+		
+	if($flag && $file != "..")
+		$flag = true;
+	else
+		$flag = false;
+		
+	if($flag && $file != '.htacess.txt')
+		$flag = true;
+	else
+		$flag = false;
+		
+	if($flag && $file != 'index.php')
+		$flag = true;
+	else
+		$flag = false;
+		
+	return $flag;	
+	
+}
+?>
 <style>
-#downloads{
+#downloads {
 	position: absolute;
-	top: 220px;
-	left: 90px;
-	text-align: center;
-	height: 226px;
+	top: 175px;
+	left: 68px;
+	text-align: left;
+	height: 290px;
 	overflow-x: hidden;
 	overflow-y: scroll;
-	width: 370px;
+	width: 421px;
 }
-#downloads a {
-	line-height:40px;
-	color:#000;
+#downloads a:hover {
+	text-decoration:none;
 }
-#downloads a:hover{
-	text-decoration:underline;
+#downloadItem {
+	color: #000000;
+	padding:5px 5px 5px 5px;
+	margin:4px 4px 4px 6px;
 }
-#downloadInfo{
-	color:#666;
-	font-size:10px;
-	position:absolute;
-	top:176px;
-	left:90px;
-	width:420px;
+#downloadItem:hover {
+	-moz-box-shadow: 0px 0px 12px #3F9BBF;
+	-webkit-box-shadow: 0px 0px 12px #3F9BBF;
+	box-shadow: 0px 0px 12px #3F9BBF;
+	background-color: #3F9BBF;
+	color: white;
+}
+.fileSize {
+	font-size:11px;
+	text-align:left;
+}
+.dateModified {
+	font-size:11px;
+	position: absolute;
+	right: 8px;
 }
 </style>
 <h2>Downloads Listing</h2>
-<span id="downloadInfo">Click on file for download. If it fails then Right click on the file and select 'Save link as'</span>
-<div id="downloads"><?php
-	$dir = './../filesForDownload/';
+<div id="downloads">
+<a href="http://home.iitb.ac.in/~sanath.kuppa/Android%20development%20kit%20complete.zip" target="_blank"><div id="downloadItem"><span class="fileName">Android development session</span><br/>
+<span class="fileSize">489.05 MB</span><span class="dateModified">23 January 2013 16:52</span></div></a>
+<?php
+	$dir = './../files/';
 	$i=0;
 	if ($handle = opendir($dir)) {
     while (false !== ($entry = readdir($handle))) {
-        if ($entry != "." && $entry != ".." && $entry != '.htacess.txt') {
-			$file[$i] = '<a href="./filesForDownload/'.$entry.'" target="downloadFrame">'.$entry.'</a><br>';//Relative path is different becoz tis file is always viewed from Index!
+        //if ($entry != "." && $entry != ".." && $entry != '.htacess.txt') {
+		if(isDownloadFile($entry)){
+			$filePath = $dir.$entry;
+			$file[$i] = '<a href="http://files.praveenkumar.co.in/'.$entry.'" target="_blank">
+			<div id="downloadItem"><span class="fileName">'.$entry.'</span><br/>
+			<span class="fileSize">'._format_bytes(filesize($filePath)).'</span>
+			<span class="dateModified">'.date ("d F Y H:i", filemtime($filePath)).'</span>
+			</div>
+			</a>';//Relative path is different becoz tis file is always viewed from Index!
 			$i++;
         }
     }
